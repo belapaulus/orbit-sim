c0f846ca823dff32a0ec52217c8abebc365ab5fc
Béla Paulus <bela.paulus1@gmail.com>
2023-02-28 23:01:42 +0000

    refactored n_body_video_changing_dt to be a function that returns the results of the simulation

--- n_body_video_changing_dt.m
+++ n_body_video_changing_dt.m
@@ -1,105 +1,92 @@
-% n body simulation with changing timestep
-% units: s, m, kg
+function [pos, vel, timestamp] = n_body_video_changing_dt(duration, dt0, N, mass, pos0, vel0) 
+% n body simulation
+% units: 
+%   s, m, kg
+% input:
+%   initial values and constants
+% side effects:
+%   creates moving figure
+% output:
+%   arrays containing the state of the system for each timestep
+%   array containing the mass of each body
 
 % constants
-% timestep 1 day = 24 * 60 * 60s
-duration = 5e7;
-dt0 = 24 * 60 * 60;
-vref = 30e3;
 G = 6.6743e-11;
-N = 3;
-mass = [2e30, 6e29, 4e24];
+vref = max(sqrt(sum(vel0.^2, 1)));
+% steps = ceil(duration/dt0);
+camera = max(pos0, [], "all"); % for creating the camera 
 
-% arrays for positions and velocities and times
-pos = zeros(1, 3, N);
-vel = zeros(1, 3, N);
-times = zeros(1);
+% arrays for positions and velocities and timestamps
+pos = zeros(N, 1, 3);
+vel = zeros(N, 1, 3);
+timestamp = zeros(1);
 
 % initial values
-vel(1, :, 1) = [0, -6923.076923076924, 0];
-vel(1, :, 2) = [0, 30e3 - 6923.076923076924, 5000];
-vel(1, :, 3) = [5000, 0, 0];
-pos(1, :, 2) = [150e9, 0, 0];
-pos(1, :, 3) = [0, 150e9, 0];
+vel(:, 1, :) = vel0;
+pos(:, 1, :) = pos0;
 
 % frame of reference
-ref = zeros(1,3);
-for i = 1:N
-	ref = ref + (squeeze(vel(1, :, i)).*mass(i));
-end
-ref = ref./sum(mass);
-for i = 1:N
-	vel(1, :, i) = vel(1, :, i) - ref;
-end
+ref = sum(vel(:, 1, :).*mass', 1) / sum(mass);
+vel(:, 1, :) = vel(:, 1, :) - ref;
 
-% create video object
-writerObj = VideoWriter("testvideo.mp4", 'MPEG-4'); % see VideoWriter docs for other formats
-writerObj.FrameRate = 30; % set frame rate for video
-open(writerObj)
 figure
 
 % run sim
 step = 1;
 time = 0;
-while time < duration
+while time < duration % && step < steps
     % calculate next timestep
-    times(step) = time;
-    maxV = max(sqrt(sum(vel(step, :, :).^2, 2)));
-    dt = dt0 * vref/maxV;
+    timestamp(step) = time;
+    maxV = max(sqrt(sum(vel(:, step, :).^2, 1)));
+    dt = dt0 * (vref/maxV); % ^2;
 
     % update ith body
 	for i = 1:N
-		vel(step + 1, :, i) = vel(step, :, i);
+		vel(i, step+1, :) = vel(i, step, :);
 		for j = 1:N
 			if i ~= j
                 % calculate distance r = |posj - posi|
-                dpos = pos(step, :, j) - pos(step, :, i);
+                dpos = pos(j, step, :) - pos(i, step, :);
 				r = sqrt(sum(dpos.^2));
-				vel(step + 1, :, i) = vel(step + 1, :, i) + dt*G*mass(j)*dpos / (r^3);
-
-				pos(step + 1, :, i) = pos(step, :, i) + dt*vel(step + 1, :, i);
+				vel(i, step + 1, :) = vel(i, step + 1, :) + dt*G*mass(j)*dpos / (r^3);
 			end
-		end
-    end    
+        end
+		pos(i, step + 1, :) = pos(i, step, :) + dt*vel(i, step + 1, :);
+    end
+    time = time + dt;
+    step = step + 1;
 
     % clear the current frame
     clf
 
     % plot the current positions and trails
+    txt = ['Step: ' num2str(step) ', time: ' num2str(time, "%.3e") 's'];
+    subtitle(txt);
     hold on
     for i = 1:N
 	    plot3( ...
-            squeeze(pos(1:step, 1, i)), ...
-            squeeze(pos(1:step, 2, i)), ...
-            squeeze(pos(1:step, 3, i)) ...
+            squeeze(pos(i, 1:step, 1)), ...
+            squeeze(pos(i, 1:step, 2)), ...
+            squeeze(pos(i, 1:step, 3)) ...
         )
         plot3( ...
-            squeeze(pos(step, 1, i)), ...
-            squeeze(pos(step, 2, i)), ...
-            squeeze(pos(step, 3, i)), ...
+            squeeze(pos(i, step, 1)), ...
+            squeeze(pos(i, step, 2)), ...
+            squeeze(pos(i, step, 3)), ...
             'r*' ...
         )
     end    
     hold off
 
-    % set axis limits so they don't change through movie frames
+    % set axis limits
     axis equal
-    xlim([-1.5*x(1),1.5*x(1)])
-    ylim([-1.5*x(1),1.5*x(1)])
-    zlim([-1.5*x(1),1.5*x(1)])
+    xlim([-10*camera,10*camera])
+    ylim([-10*camera,10*camera])
+    zlim([-10*camera,10*camera])
     view([ ...
-        0.5*x(1), ...
-        0.5*x(1), ...
-        0.2*x(1) ...
-    ])
-
-    % write this figure to the video as a new frame
-    currentframe = getframe();
-    writeVideo(writerObj, currentframe)
-
-    time = time + dt;
-    step = step + 1;
+        0.5*camera, ...
+        0.5*camera, ...
+        0.2*camera ...
+        ])
+    shg
 end
-
-% close video (saves video)
-close(writerObj)
